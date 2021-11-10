e81a33ffb25502ed90033dba056d19845c8187af
Matias Dominguez <petunilo96@gmail.com>
2021-11-10 12:44:23 -0300

    arrays and objects

--- public/app.js
+++ public/app.js
@@ -1,14 +1,30 @@
 let time = +prompt("Ingrese un horario en formato 24hs");
 let output = "";
+let timeSelected = [];
 
-function findTime() {
-  if (time == 10 || time == 12 || time == 14 || time == 20)
+class Ticket {
+  constructor(time){
+    this.time = time;
+  }
+
+  findTime(){
+    if (time == 10 || time == 12 || time == 14 || time == 20)
     return (output = "Felicidades! Ya tiene su ticket");
-  else return (output = "No hay tickets disponibles en ese horario");
+    else return (output = "No hay tickets disponibles en ese horario");
+  }
+
+  addArray(){
+    timeSelected.push(time)
+  }
+
+  printAnswer() {
+    alert(this.findTime());
+  }
 }
 
-function printAnswer() {
-  alert(findTime());
-}
+let turn = new Ticket(time);
+turn.findTime();
+turn.printAnswer();
+turn.addArray();
 
-printAnswer();
+console.log(timeSelected)
