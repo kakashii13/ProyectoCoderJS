25d66bc8986cd2bd0b90210cc0cbdc34bc4adf36
Matias Dominguez <petunilo96@gmail.com>
2021-11-26 18:28:03 -0300

    agregue interaccion con el Dom

--- public/app.js
+++ public/app.js
@@ -1,27 +1,76 @@
 let reserve = document.getElementById("btn");
 
+// function selected hour id
+function getTime() {
+  const schedule = document.querySelectorAll(".hour");
 
-function getTime(){
-  const schedule = document.querySelectorAll(".hour")
-  
+  let time = [];
+
   schedule.forEach((hour) => {
-    if(hour.checked === true){
-      console.log(hour.id)
-    }else {
-      hour.disabled = true; 
+    if (hour.checked) {
+      time.push(hour.id);
     }
-  })
+  });
+
+  return time;
 }
 
+// create alerta
+class Alert {
+  constructor(alerta) {
+    this.alerta = alerta;
+  }
+
+  alertDangerOne() {
+    this.alerta.classList.add("alert", "alert-danger", "alerta--transition");
+    this.alerta.innerText = "No puede seleccionar mas de un horario";
+    setTimeout(() => {
+      location.reload();
+    }, 2000);
+  }
+
+  alertDangerTwo() {
+    this.alerta.classList.add("alert", "alert-danger", "alerta--transition");
+    this.alerta.innerText = "Debe seleccionar un horario";
+    setTimeout(() => {
+      location.reload();
+    }, 2000);
+  }
+
+  alertSuccess() {
+    this.alerta.classList.add("alert", "alert-success", "alerta--transition");
+    this.alerta.innerText = "Felicidades ya tiene su ticket!";
+    setTimeout(() => {
+      location.reload();
+    }, 2000);
+  }
+}
+
+// function reserved
 reserve.addEventListener("click", () => {
-  let spinner = document.getElementById("spinner");
-  spinner.classList.add("spinner-border", "text-primary")
-  getTime();
-  setTimeout(() => {
-    console.log("EXCELENTE, YA TIENE SU TICKET")
-    spinner.classList.remove("spinner-border", "text-primary")
-  }, 3000)
+  // alert
+  const alert = document.querySelector("#alert");
+  // get time id
+  const time = getTime();
+
+  if (time.length !== 0) {
+    if (time.length >= 2) {
+      let alerta = new Alert(alert);
+      alerta.alertDangerOne();
+    } else {
+      // add class to spinner
+      let spinner = document.getElementById("spinner");
+      spinner.classList.add("spinner-border", "text-primary");
+
+      // function answer
+      setTimeout(() => {
+        spinner.classList.remove("spinner-border", "text-primary");
+        let alerta = new Alert(alert);
+        alerta.alertSuccess();
+      }, 1500);
+    }
+  } else {
+    let alerta = new Alert(alert);
+    alerta.alertDangerTwo();
+  }
 });
-
-
-
